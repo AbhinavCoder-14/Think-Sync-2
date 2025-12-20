e1a0e0446101798bc365b100d29b49342bd0eceb
= <kanikayadav061@gmail.com>
2025-12-21 01:13:37 +0530

    feat: integrate Socket.IO for real-time communication and update package dependencies

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,22 @@
 
 import express from 'express';
 import cors from "cors"
+import http from 'http';
+
+import { IoManager } from './manager/IoManger.js';
 
 
 const app = express()
 const port = process.env.port || 4000;
 
+
 app.use(cors())
 
 
+const io = IoManager.getSocketInstance()
+
+
+const server = http.createServer(app)
 
 
 
@@ -22,6 +30,6 @@ app.use(cors())
 
 app.listen(port,()=>{
     console.log(`Server is up and running on ${port}`)
-})
+});
 
 
---
+++ Backend/src/manager/IoManger.ts
@@ -0,0 +1,37 @@
+import { Server } from "socket.io";
+import http from "http";
+import { stringify } from "querystring";
+import type { Http2Server } from "http2";
+
+export class IoManager {
+  work: string;
+  // private static server?: Http2Server;
+  private static _io: Server;
+  public static instance: IoManager;
+
+  private constructor(server: Http2Server) {
+    this.work = "This is the class for singlton instance creation";
+
+    IoManager._io = new Server(server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+  }
+
+  public static getSocketInstance(server?: Http2Server): IoManager {
+    if (!this.instance) {
+      if (!server) {
+        throw new Error("server is not formed");
+      }
+      this.instance = new IoManager(server);
+    }
+    return this.instance;
+  }
+
+  public get io() {
+    return IoManager._io;
+  }
+}
+
